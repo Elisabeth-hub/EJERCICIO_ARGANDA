09ef6e76d94372e21c2e6469e6c25b3a0a4487f1
Alexrs26 <62642356+Alexrs26@users.noreply.github.com>
2020-04-01 09:44:05 +0200

    Add files via upload

---
+++ Ejercicio_Conjunto/Ejercicio conjunto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio_conjunto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
